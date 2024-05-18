b58d3fa018e60d763a36518525c3735ab95627ec
Jan Hrabě <janhrabe9@gmail.com>
2024-05-18 23:50:06 +0200

    Add loading UI state

--- app/src/main/java/cz/hrabe/pokedex/ui/screen/detail/DetailScreen.kt
+++ app/src/main/java/cz/hrabe/pokedex/ui/screen/detail/DetailScreen.kt
@@ -1,21 +1,40 @@
 package cz.hrabe.pokedex.ui.screen.detail
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import cz.hrabe.pokedex.domain.Pokemon
 
 @Composable
 fun DetailScreen(
-    detailScreenViewModel: DetailScreenViewModel,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    detailScreenViewModel: DetailScreenViewModel
 ) {
 
+    val uiState by detailScreenViewModel.detailUiState.collectAsState()
+
+    if (uiState.pokemon == null) {
+        Box(modifier = modifier.fillMaxSize()) {
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        }
+    } else {
+        PokemonDetail(pokemon = uiState.pokemon!!, modifier = modifier)
+    }
 }
 
-class DetailScreenViewModelFactory(private val pokemonId: Int) : ViewModelProvider.Factory {
-    @Suppress("UNCHECKED_CAST")
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return DetailScreenViewModel(pokemonId) as T
+@Composable
+fun PokemonDetail(modifier: Modifier = Modifier, pokemon: Pokemon) {
+    Column(modifier = modifier.fillMaxSize()) {
+        Text(text = pokemon.name)
+
     }
 }
