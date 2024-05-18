038e5a693bdebece0a402d89d30a3dc631ba02d0
Jan Hrabě <janhrabe9@gmail.com>
2024-05-18 21:19:11 +0200

    Create data dependency providers

---
+++ app/src/main/java/cz/hrabe/pokedex/di/DataModule.kt
@@ -0,0 +1,57 @@
+package cz.hrabe.pokedex.di
+
+import android.content.Context
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.room.Room
+import cz.hrabe.pokedex.data.PokemonRepository
+import cz.hrabe.pokedex.data.PokemonRepositoryImpl
+import cz.hrabe.pokedex.data.local.PokemonDao
+import cz.hrabe.pokedex.data.local.PokemonDatabase
+import cz.hrabe.pokedex.data.local.PokemonEntity
+import cz.hrabe.pokedex.data.remote.PokemonApi
+import cz.hrabe.pokedex.data.remote.PokemonRemoteMediator
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@OptIn(ExperimentalPagingApi::class)
+@Module
+@InstallIn(SingletonComponent::class)
+object DataModule {
+
+    @Provides
+    @Singleton
+    fun providePokemonDatabase(@ApplicationContext context: Context): PokemonDatabase {
+        return Room.databaseBuilder(context, PokemonDatabase::class.java, PokemonDatabase.NAME)
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Provides
+    fun providePokemonDao(pokemonDatabase: PokemonDatabase): PokemonDao = pokemonDatabase.pokemonDao
+
+    @Provides
+    @Singleton
+    fun providePokemonPager(
+        pokemonDatabase: PokemonDatabase,
+        pokemonApi: PokemonApi
+    ): Pager<Int, PokemonEntity> {
+        return Pager(
+            config = PagingConfig(pageSize = 20),
+            pagingSourceFactory = { pokemonDatabase.pokemonDao.pagingSource() },
+            remoteMediator = PokemonRemoteMediator(pokemonApi, pokemonDatabase)
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun providePokemonRepository(pager: Pager<Int, PokemonEntity>): PokemonRepository {
+        return PokemonRepositoryImpl(pager)
+    }
+
+}
