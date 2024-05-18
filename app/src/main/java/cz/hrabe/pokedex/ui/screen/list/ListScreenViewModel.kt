31173ea68d3cf6917985b2438276e7ed3ba72cd5
Jan Hrabě <janhrabe9@gmail.com>
2024-05-18 21:26:21 +0200

    Remove unnecessary import

--- app/src/main/java/cz/hrabe/pokedex/ui/screen/list/ListScreenViewModel.kt
+++ app/src/main/java/cz/hrabe/pokedex/ui/screen/list/ListScreenViewModel.kt
@@ -3,7 +3,6 @@ package cz.hrabe.pokedex.ui.screen.list
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
-import cz.hrabe.pokedex.data.PokemonRepository
 import cz.hrabe.pokedex.domain.GetPokemonPagingDataUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
