b74a8444faadf9f928a0690117e41e076a99b40b
Jan Hrabě <janhrabe9@gmail.com>
2024-05-18 21:17:23 +0200

    Create pokemon remote mediator

---
+++ app/src/main/java/cz/hrabe/pokedex/data/remote/PokemonRemoteMediator.kt
@@ -0,0 +1,59 @@
+package cz.hrabe.pokedex.data.remote
+
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.LoadType
+import androidx.paging.PagingState
+import androidx.paging.RemoteMediator
+import cz.hrabe.pokedex.data.local.PokemonDatabase
+import cz.hrabe.pokedex.data.local.PokemonEntity
+import cz.hrabe.pokedex.data.remote.model.pokemon.toPokemonEntity
+
+@OptIn(ExperimentalPagingApi::class)
+class PokemonRemoteMediator(
+    private val pokemonApi: PokemonApi,
+    private val pokemonDatabase: PokemonDatabase
+) : RemoteMediator<Int, PokemonEntity>() {
+    override suspend fun load(
+        loadType: LoadType,
+        state: PagingState<Int, PokemonEntity>
+    ): MediatorResult {
+        return try {
+            val pageSize = state.config.pageSize
+
+            val offset = when (loadType) {
+                LoadType.REFRESH -> 0
+                LoadType.PREPEND -> return MediatorResult.Success(
+                    endOfPaginationReached = true
+                )
+
+                LoadType.APPEND -> {
+                    val lastItem = state.lastItemOrNull()
+                    lastItem?.id ?: 0
+                }
+            }
+
+            val pokemonListing =
+                pokemonApi.fetchPokemonListing(limit = pageSize, offset = offset)
+
+            val pokemonEntities = pokemonListing.results.map {
+                val pokemonDto = pokemonApi.fetchPokemonByName(it.name)
+                pokemonDto.toPokemonEntity()
+            }
+
+            pokemonDatabase.runInTransaction {
+                val pokemonDao = pokemonDatabase.pokemonDao
+                if (loadType == LoadType.REFRESH) {
+                    pokemonDao.deleteAll()
+                }
+
+                pokemonDao.upsertAll(pokemonEntities)
+            }
+
+            MediatorResult.Success(
+                endOfPaginationReached = pokemonListing.next == null
+            )
+        } catch (e: Exception) {
+            MediatorResult.Error(e)
+        }
+    }
+}
