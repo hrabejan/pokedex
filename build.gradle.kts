4603bd6fa79d403f95af5257225df0570aaae293
Jan Hrabě <janhrabe9@gmail.com>
2024-05-18 21:12:54 +0200

    Enable serialization plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt) apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.24"
 }
