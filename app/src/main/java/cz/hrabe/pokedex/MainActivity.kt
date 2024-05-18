02164c2131979de69a9de93ff6df536c28fddc6d
Jan Hrabě <janhrabe9@gmail.com>
2024-05-18 21:23:31 +0200

    Implement navigation between list and detail

--- app/src/main/java/cz/hrabe/pokedex/MainActivity.kt
+++ app/src/main/java/cz/hrabe/pokedex/MainActivity.kt
@@ -4,44 +4,50 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
+import cz.hrabe.pokedex.ui.screen.Destination
+import cz.hrabe.pokedex.ui.screen.detail.DetailScreen
+import cz.hrabe.pokedex.ui.screen.detail.DetailScreenViewModelFactory
+import cz.hrabe.pokedex.ui.screen.list.ListScreen
+import cz.hrabe.pokedex.ui.screen.list.ListScreenViewModel
 import cz.hrabe.pokedex.ui.theme.PokeDexTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             PokeDexTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = Destination.List) {
+                    composable<Destination.List> {
+                        val listScreenViewModel =
+                            ViewModelProvider(this@MainActivity)[ListScreenViewModel::class.java]
+                        ListScreen(listScreenViewModel = listScreenViewModel) { pokemon ->
+                            navController.navigate(Destination.Detail(pokemon.id))
+                        }
+                    }
+                    composable<Destination.Detail> { backStackEntry ->
+                        val detail: Destination.Detail = backStackEntry.toRoute()
+                        DetailScreen(
+                            detailScreenViewModel = viewModel(
+                                factory = DetailScreenViewModelFactory(
+                                    detail.id
+                                )
+                            )
+                        )
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PokeDexTheme {
-        Greeting("Android")
     }
 }
