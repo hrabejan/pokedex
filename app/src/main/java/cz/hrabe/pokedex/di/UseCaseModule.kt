494cf03e28846fbe02395cc51763e884180cd9a6
Jan Hrabě <janhrabe9@gmail.com>
2024-05-18 23:46:58 +0200

    Add single pokemon use case

--- app/src/main/java/cz/hrabe/pokedex/di/UseCaseModule.kt
+++ app/src/main/java/cz/hrabe/pokedex/di/UseCaseModule.kt
@@ -1,7 +1,9 @@
 package cz.hrabe.pokedex.di
 
 import cz.hrabe.pokedex.data.PokemonRepository
+import cz.hrabe.pokedex.data.local.PokemonDao
 import cz.hrabe.pokedex.domain.GetPokemonPagingDataUseCase
+import cz.hrabe.pokedex.domain.GetSinglePokemonFromDbUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -12,5 +14,10 @@ import dagger.hilt.components.SingletonComponent
 object UseCaseModule {
 
     @Provides
-    fun provideGetPokemonPagingDataUseCase(pokemonRepository: PokemonRepository) = GetPokemonPagingDataUseCase(pokemonRepository)
+    fun provideGetPokemonPagingDataUseCase(pokemonRepository: PokemonRepository) =
+        GetPokemonPagingDataUseCase(pokemonRepository)
+
+    @Provides
+    fun provideGetSinglePokemonFromDbUseCase(pokemonDao: PokemonDao) =
+        GetSinglePokemonFromDbUseCase(pokemonDao)
 }
--- app/src/main/java/cz/hrabe/pokedex/domain/GetSinglePokemonFromDbUseCase.kt
+++ app/src/main/java/cz/hrabe/pokedex/domain/GetSinglePokemonFromDbUseCase.kt
@@ -1,10 +1,12 @@
 package cz.hrabe.pokedex.domain
 
 import cz.hrabe.pokedex.data.local.PokemonDao
+import cz.hrabe.pokedex.data.local.toPokemon
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class GetSinglePokemonFromDbUseCase @Inject constructor(private val pokemonDao: PokemonDao) {
 
-    operator fun invoke(pokemonId: Int): Flow<Pokemon> = pokemonDao.getPokemonById(pokemonId)
+    operator fun invoke(pokemonId: Int): Flow<Pokemon> = pokemonDao.getPokemonById(pokemonId).map { it.toPokemon() }
 }
