ae433002cbf28bfccc3dcd55e272afeef793ef43
Jan Hrabě <janhrabe9@gmail.com>
2024-05-19 19:08:01 +0200

    Basic data visualization

--- app/src/main/java/cz/hrabe/pokedex/ui/screen/detail/DetailScreen.kt
+++ app/src/main/java/cz/hrabe/pokedex/ui/screen/detail/DetailScreen.kt
@@ -1,9 +1,13 @@
 package cz.hrabe.pokedex.ui.screen.detail
 
+import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -13,6 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import cz.hrabe.pokedex.domain.Pokemon
+import cz.hrabe.pokedex.ui.screen.components.TypeList
 
 @Composable
 fun DetailScreen(
@@ -22,12 +27,14 @@ fun DetailScreen(
 
     val uiState by detailScreenViewModel.detailUiState.collectAsState()
 
-    if (uiState.pokemon == null) {
-        Box(modifier = modifier.fillMaxSize()) {
-            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+    Scaffold {
+        if (uiState.pokemon == null) {
+            Box(modifier = modifier.fillMaxSize().padding(it)) {
+                CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+            }
+        } else {
+            PokemonDetail(pokemon = uiState.pokemon!!, modifier = modifier.padding(it))
         }
-    } else {
-        PokemonDetail(pokemon = uiState.pokemon!!, modifier = modifier)
     }
 }
 
@@ -35,6 +42,10 @@ fun DetailScreen(
 fun PokemonDetail(modifier: Modifier = Modifier, pokemon: Pokemon) {
     Column(modifier = modifier.fillMaxSize()) {
         Text(text = pokemon.name)
-
+        TypeList(
+            types = pokemon.types,
+            orientation = Orientation.Horizontal,
+            modifier = Modifier.fillMaxWidth()
+        )
     }
 }
