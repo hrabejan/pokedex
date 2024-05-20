81d1c8460fec5678ff66094986dff460326dd51f
Jan Hrabě <janhrabe9@gmail.com>
2024-05-20 22:37:27 +0200

    Add avg color field

--- app/src/main/java/cz/hrabe/pokedex/domain/Pokemon.kt
+++ app/src/main/java/cz/hrabe/pokedex/domain/Pokemon.kt
@@ -1,10 +1,13 @@
 package cz.hrabe.pokedex.domain
 
+import androidx.compose.ui.graphics.Color
+
 data class Pokemon(
     val id: Int,
     val name: String,
     val imageUrl: String,
     val height: Int,
     val weight: Int,
-    val types: List<String>
+    val types: List<String>,
+    val averageColor: Int?
 )
