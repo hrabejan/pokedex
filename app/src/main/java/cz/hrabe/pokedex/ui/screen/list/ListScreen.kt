9bfe139b5b42186f3a6ce9b9d2f3dd0ad17470e5
Jan Hrabě <janhrabe9@gmail.com>
2024-05-20 22:42:19 +0200

    Update and clean ListScreen's content

--- app/src/main/java/cz/hrabe/pokedex/ui/screen/list/ListScreen.kt
+++ app/src/main/java/cz/hrabe/pokedex/ui/screen/list/ListScreen.kt
@@ -1,38 +1,44 @@
 package cz.hrabe.pokedex.ui.screen.list
 
-import androidx.compose.foundation.Image
+import android.graphics.drawable.Drawable
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
@@ -43,9 +49,12 @@ import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import cz.hrabe.pokedex.R
 import cz.hrabe.pokedex.domain.Pokemon
+import cz.hrabe.pokedex.domain.utils.getContrastColor
 import cz.hrabe.pokedex.ui.screen.components.NumberHeader
 import cz.hrabe.pokedex.ui.screen.components.TypeList
+import cz.hrabe.pokedex.ui.theme.spacing
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ListScreen(
     listScreenViewModel: ListScreenViewModel,
@@ -55,7 +64,25 @@ fun ListScreen(
     val pokemonPagingItems = listScreenViewModel.pokemon.collectAsLazyPagingItems()
     val perRow = 2
 
-    Scaffold(modifier = modifier) {
+    Scaffold(modifier = modifier
+        .fillMaxSize()
+        .background(MaterialTheme.colorScheme.background)
+        .drawPokeballBehind(getSize = { hR, wR ->
+            val width = size.width / 2f
+            Size(width, width * hR)
+        }, getOffset = { drawSize ->
+            Offset(size.width - drawSize.width, 0f)
+        }, color = MaterialTheme.colorScheme.onBackground),
+        containerColor = Color.Transparent,
+        topBar = {
+            TopAppBar(title = {
+                Text(
+                    text = "Pokedex",
+                    style = MaterialTheme.typography.headlineMedium,
+                    fontWeight = FontWeight.ExtraBold
+                )
+            }, colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.Transparent))
+        }) {
         Box(
             modifier = Modifier
                 .fillMaxSize()
@@ -64,7 +91,14 @@ fun ListScreen(
             if (pokemonPagingItems.loadState.refresh is LoadState.Loading) {
                 CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
             } else {
-                LazyVerticalGrid(columns = GridCells.Fixed(perRow)) {
+                LazyVerticalGrid(
+                    modifier = Modifier.padding(
+                        start = MaterialTheme.spacing.large,
+                        end = MaterialTheme.spacing.large,
+                        top = MaterialTheme.spacing.large
+                    ),
+                    columns = GridCells.Fixed(perRow)
+                ) {
                     items(
                         count = pokemonPagingItems.itemCount,
                         key = pokemonPagingItems.itemKey { it.id }) { index ->
@@ -74,34 +108,40 @@ fun ListScreen(
                                 pokemon = it,
                                 modifier = Modifier
                                     .fillMaxWidth(1f / perRow)
-                                    .height(200.dp), onClick = onClick
+                                    .wrapContentHeight(), onClick = onClick,
+                                onImageLoaded = {
+                                    listScreenViewModel.onImageLoaded(pokemon, it)
+                                }
                             )
                         }
                     }
                 }
             }
         }
-
     }
+
 }
 
 @Composable
-fun PokemonItem(pokemon: Pokemon, modifier: Modifier = Modifier, onClick: (Pokemon) -> Unit) {
-    Card(
-        elevation = CardDefaults.elevatedCardElevation(), modifier = modifier
-            .padding(8.dp)
-    ) {
+fun PokemonItem(
+    pokemon: Pokemon,
+    modifier: Modifier = Modifier,
+    onImageLoaded: (Drawable) -> Unit = {},
+    onClick: (Pokemon) -> Unit
+) {
+    val avgColor = pokemon.averageColor?.let { Color(it) } ?: MaterialTheme.colorScheme.surface
+    val contrastColor = avgColor.getContrastColor()
+    PokemonItemCard(modifier = modifier, pokemon = pokemon, color = avgColor, onClick = onClick) {
         ConstraintLayout(
             modifier = Modifier
                 .fillMaxSize()
-                .clickable { onClick(pokemon) }
+                .padding(MaterialTheme.spacing.mediumLarge)
         ) {
-            val (number, col, image, backgroundImg, name) = createRefs()
+            val (number, col, name, image) = createRefs()
 
             NumberHeader(number = pokemon.id,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(end = 16.dp, top = 16.dp)
                     .constrainAs(number) {
                         top.linkTo(parent.top)
                         end.linkTo(parent.end)
@@ -114,14 +154,15 @@ fun PokemonItem(pokemon: Pokemon, modifier: Modifier = Modifier, onClick: (Pokem
                 text = pokemon.name.replaceFirstChar { it.uppercase() },
                 style = TextStyle(
                     fontWeight = FontWeight.ExtraBold,
-                    fontSize = MaterialTheme.typography.bodyLarge.fontSize
+                    fontSize = MaterialTheme.typography.bodyLarge.fontSize,
+                    color = contrastColor
                 ),
                 modifier = Modifier
                     .constrainAs(name) {
                         top.linkTo(number.bottom)
                         start.linkTo(parent.start)
                     }
-                    .padding(start = 16.dp, bottom = 16.dp)
+                    .padding(bottom = MaterialTheme.spacing.medium)
                     .zIndex(2f)
             )
 
@@ -129,42 +170,94 @@ fun PokemonItem(pokemon: Pokemon, modifier: Modifier = Modifier, onClick: (Pokem
                 .constrainAs(col) {
                     top.linkTo(name.bottom)
                     start.linkTo(parent.start)
-                    bottom.linkTo(parent.bottom)
-                    height = Dimension.fillToConstraints
                 }
-                .padding(start = 16.dp, bottom = 16.dp)
-                .zIndex(2f))
-
+                .zIndex(2f),
+                textStyle = TextStyle(
+                    color = contrastColor,
+                    fontSize = 8.sp
+                ))
             AsyncImage(
                 model = ImageRequest.Builder(LocalContext.current)
                     .data(pokemon.imageUrl)
                     .crossfade(true)
+                    .allowHardware(false)
+//                        .memoryCacheKey("${pokemon.id}")
                     .build(),
                 contentDescription = "Image of ${pokemon.name}",
-//                placeholder = R.drawable.pokemonImagePlaceholder,
                 contentScale = ContentScale.FillHeight,
-                modifier = Modifier
-                    .constrainAs(image) {
-                        top.linkTo(name.bottom)
-                        bottom.linkTo(parent.bottom)
-                        end.linkTo(parent.end)
-                        start.linkTo(col.end)
-                        height = Dimension.fillToConstraints
-                    }
-                    .zIndex(1f)
-            )
-            Image(
-                painter = painterResource(id = R.drawable.pokeball),
-                contentDescription = "Background pokeball",
-                colorFilter = ColorFilter.tint(Color.White.copy(alpha = 0.1f)),
-                modifier = Modifier.constrainAs(backgroundImg) {
+                modifier = Modifier.constrainAs(image) {
                     top.linkTo(name.bottom)
-                    bottom.linkTo(parent.bottom)
                     end.linkTo(parent.end)
-                    height = Dimension.fillToConstraints
-                }, contentScale = ContentScale.FillHeight
+                    bottom.linkTo(parent.bottom)
+                    height = Dimension.value(80.dp)
+                },
+                onSuccess = {
+                    onImageLoaded(it.result.drawable)
+                }
             )
         }
     }
 }
 
+
+@Composable
+fun PokemonItemCard(
+    modifier: Modifier = Modifier,
+    pokemon: Pokemon,
+    onClick: (Pokemon) -> Unit,
+    color: Color,
+    content: @Composable BoxScope.() -> Unit
+) {
+    Card(
+        elevation = CardDefaults.elevatedCardElevation(defaultElevation = 10.dp),
+        modifier = modifier
+            .padding(MaterialTheme.spacing.mediumSmall)
+    ) {
+        Surface(
+            modifier = Modifier
+                .clip(CardDefaults.shape)
+                .clickable { onClick(pokemon) }, color = color
+        ) {
+
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .drawPokeballBehind(getSize = { hR, _ ->
+                        val width = size.width / 1.8f
+                        Size(width, width * hR)
+                    }, getOffset = { newSize ->
+                        Offset(size.width - newSize.width, size.height - newSize.height)
+                    }, color = Color.White)
+            ) {
+                content()
+            }
+        }
+    }
+}
+
+@Composable
+fun Modifier.drawPokeballBehind(
+    color: Color = Color.Black,
+    alpha: Float = 0.2f,
+    getSize: DrawScope.(heightRatio: Float, widthRatio: Float) -> Size,
+    getOffset: DrawScope.(size: Size) -> Offset
+): Modifier {
+
+    val painter = painterResource(id = R.drawable.pokeball)
+    return this then Modifier.drawBehind {
+        with(painter) {
+            val heightRatio = intrinsicSize.height / intrinsicSize.width
+            val widthRatio = intrinsicSize.width / intrinsicSize.height
+            val newSize = getSize(heightRatio, widthRatio)
+            val offset = getOffset(newSize)
+            translate(left = offset.x, top = offset.y) {
+                draw(
+                    size = Size(newSize.width, newSize.height),
+                    colorFilter = ColorFilter.tint(color),
+                    alpha = alpha
+                )
+            }
+        }
+    }
+}
