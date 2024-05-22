e9b8054fc261fdc59a54a7f04b9f786e8b722d00
Jan Hrabě <janhrabe9@gmail.com>
2024-05-23 00:37:44 +0200

    Add height and weight helper functions

--- app/src/main/java/cz/hrabe/pokedex/domain/Pokemon.kt
+++ app/src/main/java/cz/hrabe/pokedex/domain/Pokemon.kt
@@ -1,6 +1,6 @@
 package cz.hrabe.pokedex.domain
 
-import androidx.compose.ui.graphics.Color
+import java.math.RoundingMode
 
 data class Pokemon(
     val id: Int,
@@ -10,4 +10,34 @@ data class Pokemon(
     val weight: Int,
     val types: List<String>,
     val averageColor: Int?
-)
+) {
+
+    val weightKg: Double
+        get() = weight / 10.0
+
+    fun weightLbs(decimals: Int = 1, roundingMode: RoundingMode = RoundingMode.HALF_UP): Double {
+        val lbs = weightKg * 2.205
+        val roundedLbs =
+            lbs.toBigDecimal().setScale(decimals, roundingMode).toDouble()
+        return roundedLbs
+    }
+
+    val heightIn: Double
+        get() = heightCm / 2.54
+
+    val heightCm: Double
+        get() = height * 10.0
+
+    /**
+     * Return's a text representation of height in feet and inches
+     *
+     * Example: 2' 7"
+     */
+    fun heightFtIn(): String {
+        val feet = (heightIn / 12).toInt()
+        val leftover = heightIn % 12
+
+        return "$feet\' ${"%.1f".format(leftover)}\""
+    }
+
+}
