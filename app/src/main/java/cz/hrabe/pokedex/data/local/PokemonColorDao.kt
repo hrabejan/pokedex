a7a03c8944b721b3b8d389294b74d584a686557f
Jan Hrabě <janhrabe9@gmail.com>
2024-05-23 01:18:09 +0200

    Create a dao for color objects

---
+++ app/src/main/java/cz/hrabe/pokedex/data/local/PokemonColorDao.kt
@@ -0,0 +1,19 @@
+package cz.hrabe.pokedex.data.local
+
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface PokemonColorDao {
+
+    @Upsert
+    fun upsert(pokemonColorEntity: PokemonColorEntity)
+
+    @Query("DELETE FROM pokemon_color")
+    fun deleteAll()
+
+    @Query("SELECT * FROM pokemon_color WHERE pokemon_id=:pokemonId")
+    fun getPokemonColor(pokemonId: Int): Flow<PokemonColorEntity>
+}
