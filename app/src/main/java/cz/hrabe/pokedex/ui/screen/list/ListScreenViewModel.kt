6c1f516a622d563f6788978c33e7ccd88b84a85d
Jan Hrabě <janhrabe9@gmail.com>
2024-05-23 02:02:49 +0200

    Add pokemons colors use case

--- app/src/main/java/cz/hrabe/pokedex/ui/screen/list/ListScreenViewModel.kt
+++ app/src/main/java/cz/hrabe/pokedex/ui/screen/list/ListScreenViewModel.kt
@@ -4,21 +4,31 @@ import android.graphics.drawable.Drawable
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
+import cz.hrabe.pokedex.data.local.PokemonColorEntity
+import cz.hrabe.pokedex.data.local.toPokemonColors
 import cz.hrabe.pokedex.domain.GetPokemonPagingDataUseCase
+import cz.hrabe.pokedex.domain.GetPokemonsColorsUseCase
 import cz.hrabe.pokedex.domain.Pokemon
+import cz.hrabe.pokedex.domain.PokemonColors
 import cz.hrabe.pokedex.domain.UpdatePokemonsColorsUserCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ListScreenViewModel @Inject constructor(
     getPokemonPagingDataUseCase: GetPokemonPagingDataUseCase,
-    private val updatePokemonsColorsUserCase: UpdatePokemonsColorsUserCase
+    private val updatePokemonsColorsUserCase: UpdatePokemonsColorsUserCase,
+    private val getPokemonsColorsUseCase: GetPokemonsColorsUseCase
 ) :
     ViewModel() {
 
     val pokemon = getPokemonPagingDataUseCase().cachedIn(viewModelScope)
+
+    fun getPokemonsColors(pokemon: Pokemon): Flow<PokemonColors> =
+        getPokemonsColorsUseCase(pokemon.id)
 
     fun onImageLoaded(pokemon: Pokemon, drawable: Drawable) {
         viewModelScope.launch {
