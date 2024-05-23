8a1a132c37e533b9b4f2e971a74388b13ffb8bb0
Jan Hrabě <janhrabe9@gmail.com>
2024-05-23 18:12:49 +0200

    Remove LocalPokemonColors

--- app/src/main/java/cz/hrabe/pokedex/ui/screen/components/PokemonColors.kt
+++
@@ -1,8 +0,0 @@
-package cz.hrabe.pokedex.ui.screen.components
-
-import androidx.compose.runtime.compositionLocalOf
-import androidx.compose.ui.graphics.Color
-
-data class PokemonColors(val avgColor: Color = Color.White, val contrastColor: Color = Color.Black)
-
-val LocalPokemonColors = compositionLocalOf { PokemonColors() }
