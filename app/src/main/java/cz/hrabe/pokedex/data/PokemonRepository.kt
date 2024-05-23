bdc372344624493ecd97cc3f2a2419845e26dc0e
Jan Hrabě <janhrabe9@gmail.com>
2024-05-23 16:48:19 +0200

    Add function to fetch pokemon with colors

--- app/src/main/java/cz/hrabe/pokedex/data/PokemonRepository.kt
+++ app/src/main/java/cz/hrabe/pokedex/data/PokemonRepository.kt
@@ -2,9 +2,11 @@ package cz.hrabe.pokedex.data
 
 import androidx.paging.PagingData
 import cz.hrabe.pokedex.domain.Pokemon
+import cz.hrabe.pokedex.domain.PokemonWithColors
 import kotlinx.coroutines.flow.Flow
 
 interface PokemonRepository {
 
     fun fetchPokemon(): Flow<PagingData<Pokemon>>
+    fun fetchPokemonWithColors(): Flow<PagingData<PokemonWithColors>>
 }
