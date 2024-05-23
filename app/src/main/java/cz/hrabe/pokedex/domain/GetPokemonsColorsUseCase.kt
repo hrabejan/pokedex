b9785ba9a6ab5767923e3ee0da1685af8ef91604
Jan Hrabě <janhrabe9@gmail.com>
2024-05-23 18:12:32 +0200

    Return not null and distinct

--- app/src/main/java/cz/hrabe/pokedex/domain/GetPokemonsColorsUseCase.kt
+++ app/src/main/java/cz/hrabe/pokedex/domain/GetPokemonsColorsUseCase.kt
@@ -3,9 +3,14 @@ package cz.hrabe.pokedex.domain
 import cz.hrabe.pokedex.data.local.PokemonColorDao
 import cz.hrabe.pokedex.data.local.toPokemonColors
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 
 class GetPokemonsColorsUseCase(private val pokemonColorDao: PokemonColorDao) {
 
-    operator fun invoke(pokemonId: Int) : Flow<PokemonColors> = pokemonColorDao.getPokemonColor(pokemonId).map { it.toPokemonColors() }
+    operator fun invoke(pokemonId: Int): Flow<PokemonColors> =
+        pokemonColorDao.getPokemonColor(pokemonId).filterNotNull().distinctUntilChanged().map {
+            it.toPokemonColors()
+        }
 }
