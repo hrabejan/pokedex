3a5fbb894c4a0ad74f6165762c556ad931727f14
Jan Hrabě <janhrabe9@gmail.com>
2024-05-23 17:24:30 +0200

    Add documentation

--- app/src/main/java/cz/hrabe/pokedex/ui/screen/Destination.kt
+++ app/src/main/java/cz/hrabe/pokedex/ui/screen/Destination.kt
@@ -2,9 +2,20 @@ package cz.hrabe.pokedex.ui.screen
 
 import kotlinx.serialization.Serializable
 
+/**
+ * Main destinations of Pokedex application
+ */
 sealed class Destination {
+    /**
+     * Meant for displaying a list of all Pokemon
+     */
     @Serializable
     data object List : Destination()
+
+    /**
+     * Meant for displaying single Pokemon's details
+     * @param id pokemon's id
+     */
     @Serializable
     data class Detail(val id: Int) : Destination()
 }
