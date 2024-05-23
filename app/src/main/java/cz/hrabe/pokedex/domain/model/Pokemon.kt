418516a6ec49472cfa508531478378458292e253
Jan Hrabě <janhrabe9@gmail.com>
2024-05-23 19:06:39 +0200

    Add documentation

--- app/src/main/java/cz/hrabe/pokedex/domain/model/Pokemon.kt
+++ app/src/main/java/cz/hrabe/pokedex/domain/model/Pokemon.kt
@@ -3,18 +3,45 @@ package cz.hrabe.pokedex.domain.model
 import java.math.RoundingMode
 
 data class Pokemon(
+    /**
+     * Pokemon's identifier
+     */
     val id: Int,
+    /**
+     * Pokemon's name
+     */
     val name: String,
+    /**
+     * URL pointing to an image of the Pokemon
+     */
     val imageUrl: String,
+    /**
+     * Height in decimeters
+     */
     val height: Int,
+    /**
+     * Weight in hectograms
+     */
     val weight: Int,
+    /**
+     * Pokemon's type tags
+     */
     val types: List<String>,
+    /**
+     * Base experience gained for defeating the Pokemon
+     */
     val baseExperience: Int
 ) {
 
+    /**
+     * Pokemon's weight in kilograms
+     */
     val weightKg: Double
         get() = weight / 10.0
 
+    /**
+     *
+     */
     fun weightLbs(decimals: Int = 1, roundingMode: RoundingMode = RoundingMode.HALF_UP): Double {
         val lbs = weightKg * 2.205
         val roundedLbs =
@@ -22,9 +49,15 @@ data class Pokemon(
         return roundedLbs
     }
 
+    /**
+     * Pokemon's height in inches
+     */
     val heightIn: Double
         get() = heightCm / 2.54
 
+    /**
+     * Pokemon's height in centimeters
+     */
     val heightCm: Double
         get() = height * 10.0
 
