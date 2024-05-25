aebe3dd3b448a362778643a2458b99c2734ee3fc
Jan Hrabě <janhrabe9@gmail.com>
2024-05-25 18:12:03 +0200

    Add documentation

--- app/src/main/java/cz/hrabe/pokedex/domain/GetPokemonsContrastColorUseCase.kt
+++ app/src/main/java/cz/hrabe/pokedex/domain/GetPokemonsContrastColorUseCase.kt
@@ -7,6 +7,14 @@ import androidx.core.graphics.ColorUtils
 
 class GetPokemonsContrastColorUseCase {
 
+    /**
+     * Returns the [onDark] or [onLight] ColorInt based on the luminance of [color].
+     * If the luminance equal or bigger than 0.5, [onLight] is returned, otherwise [onDark].
+     *
+     * @param color color to get the contrast color for
+     * @param onDark return value in case of dark luminance
+     * @param onLight return value in case of light luminance
+     */
     operator fun invoke(
         @ColorInt color: Int,
         @ColorInt onDark: Int = Color.White.toArgb(),
