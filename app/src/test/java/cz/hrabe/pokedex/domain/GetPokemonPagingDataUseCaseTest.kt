da20945ceacbe536d43f157163706263a6ad9c8c
Jan Hrabě <janhrabe9@gmail.com>
2024-05-25 14:26:11 +0200

    Implement tests

--- app/src/test/java/cz/hrabe/pokedex/domain/GetPokemonPagingDataUseCaseTest.kt
+++ app/src/test/java/cz/hrabe/pokedex/domain/GetPokemonPagingDataUseCaseTest.kt
@@ -1,9 +1,30 @@
 package cz.hrabe.pokedex.domain
 
-import org.junit.Assert.*
+import cz.hrabe.pokedex.data.PokemonRepository
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
 
 class GetPokemonPagingDataUseCaseTest {
-     fun shouldCallFetchPokemonWithColorsOnce() {
+    private lateinit var repository: PokemonRepository
+    private lateinit var useCase: GetPokemonPagingDataUseCase
 
-     }
+    @Before
+    fun setUp() {
+        repository = mockk<PokemonRepository>()
+        useCase = GetPokemonPagingDataUseCase(repository)
+    }
+
+    @Test
+    fun shouldCallFetchPokemonWithColorsOnce() = runTest {
+        every { repository.fetchPokemonWithColors() } returns emptyFlow()
+        useCase.invoke()
+
+        verify(atMost = 1) { repository.fetchPokemonWithColors() }
+    }
 }
