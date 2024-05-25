4afff35fbc700aeee933409e176d80089065ba54
Jan Hrabě <janhrabe9@gmail.com>
2024-05-25 15:38:56 +0200

    Add documentation

--- app/src/main/java/cz/hrabe/pokedex/domain/model/PokemonColors.kt
+++ app/src/main/java/cz/hrabe/pokedex/domain/model/PokemonColors.kt
@@ -2,6 +2,9 @@ package cz.hrabe.pokedex.domain.model
 
 import androidx.compose.ui.graphics.Color
 
+/**
+ * Base UI model for Pokemon's color scheme
+ */
 class PokemonColors(
     val averageColor: Color,
     val contrastColor: Color
--- app/src/main/java/cz/hrabe/pokedex/domain/model/PokemonWithColors.kt
+++ app/src/main/java/cz/hrabe/pokedex/domain/model/PokemonWithColors.kt
@@ -1,5 +1,8 @@
 package cz.hrabe.pokedex.domain.model
 
+/**
+ * UI query model of a Pokemon and it's color scheme
+ */
 data class PokemonWithColors(
     val pokemon: Pokemon,
     val pokemonColors: PokemonColors?
