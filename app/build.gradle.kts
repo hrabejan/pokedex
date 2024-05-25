d846d678c0cbe755369964af01d5278e75148fed
Jan Hrabě <janhrabe9@gmail.com>
2024-05-25 14:25:52 +0200

    Add Mockk and Coroutines test dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,8 @@ dependencies {
     implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     testImplementation(libs.junit.v412)
+    testImplementation(libs.mockk)
+    testImplementation(libs.kotlinx.coroutines.test)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
