984fe739cb60d909a50b35a8d131e1b3e6723850
Jan Hrabě <janhrabe9@gmail.com>
2024-05-25 14:54:34 +0200

    Add documentation

--- app/src/main/java/cz/hrabe/pokedex/data/remote/PokemonApi.kt
+++ app/src/main/java/cz/hrabe/pokedex/data/remote/PokemonApi.kt
@@ -6,18 +6,35 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
+/**
+ * Retrofit interface defining basic functions for fetching Pokemon
+ */
 interface PokemonApi {
 
+    /**
+     * Fetches all Pokemon [limit] of Pokemon starting from ID [offset] (exclusive, first Pokemon's ID is 1)
+     *
+     * @param limit how many Pokemon per request
+     * @param offset ID offset of requested Pokemon (exclusive)
+     */
     @GET("pokemon/")
     suspend fun fetchPokemonListing(
         @Query("limit") limit: Int,
         @Query("offset") offset: Int
     ): PokemonListingDto
 
+    /**
+     * Fetches a single Pokemon from the API, whose name matches the argument
+     *
+     * @param name searched Pokemon's name
+     */
     @GET("pokemon/{name}")
     suspend fun fetchPokemonByName(@Path("name") name: String): PokemonDto
 
     companion object {
+        /**
+         * Base URL for fetching Pokemon data
+         */
         const val BASE_URL = "https://pokeapi.co/api/v2/"
     }
 }
