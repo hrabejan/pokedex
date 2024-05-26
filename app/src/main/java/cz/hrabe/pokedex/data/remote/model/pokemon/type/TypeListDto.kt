ed70876ab1f6102a66273231ae163c633288c67d
Jan Hrabě <janhrabe9@gmail.com>
2024-05-26 18:11:07 +0200

    Delete useless TypeListDto

--- app/src/main/java/cz/hrabe/pokedex/data/remote/model/pokemon/type/TypeListDto.kt
+++
@@ -1,6 +0,0 @@
-package cz.hrabe.pokedex.data.remote.model.pokemon.type
-
-import com.squareup.moshi.JsonClass
-
-@JsonClass(generateAdapter = true)
-data class TypeListDto(val types: List<TypeDto>)
