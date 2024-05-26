18749ac555f8191a4eb2ab01d1fe3029b2a80783
Jan Hrabě <janhrabe9@gmail.com>
2024-05-26 17:55:59 +0200

    Add documentation

--- app/src/main/java/cz/hrabe/pokedex/domain/model/PokemonColors.kt
+++ app/src/main/java/cz/hrabe/pokedex/domain/model/PokemonColors.kt
@@ -6,6 +6,12 @@ import androidx.compose.ui.graphics.Color
  * Base UI model for Pokemon's color scheme
  */
 class PokemonColors(
+    /**
+     * Average color of Pokemon's downloaded image
+     */
     val averageColor: Color,
+    /**
+     * Contrast color of [averageColor]
+     */
     val contrastColor: Color
 )
