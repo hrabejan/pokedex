3f29b92518b2b40ddfd8d0530e808f55d9050e12
Jan Hrabě <janhrabe9@gmail.com>
2024-05-26 18:47:33 +0200

    Add a progress bar at the bottom of the list when items are loading

--- app/src/main/java/cz/hrabe/pokedex/ui/screen/list/ListScreen.kt
+++ app/src/main/java/cz/hrabe/pokedex/ui/screen/list/ListScreen.kt
@@ -10,11 +10,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
@@ -146,6 +148,16 @@ fun ListScreen(
                                 pokemonColors = pokemonWColors.pokemonColors ?: PokemonColors(
                                     MaterialTheme.colorScheme.surface,
                                     MaterialTheme.colorScheme.onSurface
+                                )
+                            )
+                        }
+                    }
+                    if (pokemonPagingItems.loadState.append is LoadState.Loading) {
+                        item(span = { GridItemSpan(maxLineSpan) }) {
+                            LinearProgressIndicator(
+                                modifier = Modifier.padding(
+                                    vertical = MaterialTheme.spacing.medium,
+                                    horizontal = MaterialTheme.spacing.large
                                 )
                             )
                         }
