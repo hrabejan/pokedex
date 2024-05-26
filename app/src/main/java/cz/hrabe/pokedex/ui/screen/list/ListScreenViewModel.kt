f535f0090714cda4b941d6a69ce956cf1ec64974
Jan Hrabě <janhrabe9@gmail.com>
2024-05-26 17:54:12 +0200

    Add documentation

--- app/src/main/java/cz/hrabe/pokedex/ui/screen/list/ListScreenViewModel.kt
+++ app/src/main/java/cz/hrabe/pokedex/ui/screen/list/ListScreenViewModel.kt
@@ -15,11 +15,22 @@ import javax.inject.Inject
 class ListScreenViewModel @Inject constructor(
     getPokemonWithColorsPagingDataUseCase: GetPokemonWithColorsPagingDataUseCase,
     private val updatePokemonsColorsUserCase: UpdatePokemonsColorsUserCase,
-) :
-    ViewModel() {
+) : ViewModel() {
 
+    /**
+     * Paginaton flow of Pokemon with colors.
+     * Cached in the ViewModel scope, so the pagination is not reloaded on configuration change.
+     */
     val pokemon = getPokemonWithColorsPagingDataUseCase().cachedIn(viewModelScope)
 
+    /**
+     * Takes in a pokemon for it's ID and a drawable of the image that has been loaded.
+     * The drawable will be used to calculate it's average and contrast color.
+     * That result will then be saved into a local database with it's respective Pokemon id.
+     *
+     * @param pokemon Pokemon for which the image has been loaded, used only for it's ID.
+     * @param drawable Loaded image of a Pokemon.
+     */
     fun onImageLoaded(pokemon: Pokemon, drawable: Drawable) {
         viewModelScope.launch {
             updatePokemonsColorsUserCase(pokemon.id, drawable)
